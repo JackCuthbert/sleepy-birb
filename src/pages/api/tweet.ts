600dbef60c735f1cd2037c0d53b41afe7b34179a
Jack Cuthbert <jckcthbrt@gmail.com>
2020-07-17 21:01:44 +1000

    style: remove unused code, clean up api

--- src/pages/api/tweet.ts
+++ src/pages/api/tweet.ts
@@ -3,17 +3,6 @@ import cheerio from 'cheerio'
 import { NextApiRequest, NextApiResponse, NextApiHandler } from 'next'
 import { TWITTER_SCRAPE_URL } from '../../config'
 
-export interface TweetInfo {
-  author: string
-  authorImage: string
-  isReply: boolean
-  hasMedia: boolean
-  opUrl?: string
-  mediaUrl?: string
-  meta: string
-  content: string
-}
-
 export interface TweetResponse {
   author: {
     handle: string
@@ -36,61 +25,59 @@ export interface TweetResponse {
 
 function validateRequest(handler: NextApiHandler): NextApiHandler {
   return (req, res) => {
-    if (req.method !== 'POST') {
-      return res.status(404).end()
-    }
+    if (req.method !== 'POST') return res.status(404).end()
 
-    if (req.body.userId === undefined) {
+    if (req.body.userId === undefined)
       return res.status(400).send('No "userId" property')
-    }
 
-    if (req.body.statusId === undefined) {
+    if (req.body.statusId === undefined)
       return res.status(400).send('No "statusId" property')
-    }
 
     return handler(req, res)
   }
 }
 
-async function tweet(req: NextApiRequest, res: NextApiResponse): Promise<any> {
+async function tweet(
+  req: NextApiRequest,
+  res: NextApiResponse<TweetResponse>
+): Promise<any> {
   const userId = req.body.userId as string
   const statusId = req.body.statusId as string
 
-  const { data: pageHtml } = await axios.get<string>(
+  const { data: html } = await axios.get<string>(
     `${TWITTER_SCRAPE_URL}/${userId}/status/${statusId}`
   )
-  const $ = cheerio.load(pageHtml)
 
-  const tweetTextContainer = $('.tweet-detail .tweet-text').html()
-  const tweetMetadataContainer = $('.tweet-detail .metadata').html()
+  const $ = cheerio.load(html)
 
-  // author
-  const authorName = $('.tweet-detail .user-info .fullname').text()
-  const authorImage = $('.tweet-detail .avatar img').attr('src')
+  const tweetText = $('.tweet-detail .tweet-text')
+  const tweetMetadata = $('.tweet-detail .metadata')
+
+  const authorName = $('.tweet-detail .user-info .fullname')
+  const authorImage = $('.tweet-detail .avatar img')
 
   const response: TweetResponse = {
     author: {
       handle: userId,
-      image: authorImage,
-      name: authorName.replace(/\\n/gi, '').trim(),
+      image: authorImage.attr('src'),
+      name: authorName.text().replace(/\\n/gi, '').trim(),
       url: `https://twitter.com/${userId}`
     },
     tweet: {
-      content: $(tweetTextContainer).text().trim(),
-      meta: $(tweetMetadataContainer).text().trim(),
+      content: tweetText.text().trim(),
+      meta: tweetMetadata.text().trim(),
       url: `https://twitter.com/${userId}/status/${statusId}`
     }
   }
 
-  const hasParent = $('.timeline.inreplytos table').length !== 0
   const hasMedia = $('.tweet-detail .media img').length !== 0
-
   if (hasMedia) {
     response.media = {
       url: $('.tweet-detail .media img').attr('src').replace(':small', '')
     }
   }
 
+  const hasParent = $('.timeline.inreplytos table').length !== 0
   if (hasParent) {
     response.parent = {
       url: `https://twitter.com${$('.timeline.inreplytos table')
